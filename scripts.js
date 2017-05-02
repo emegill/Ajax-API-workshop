a49df8f064ec4ca1ad8d512a2ad5c41de80b0538
Ethan Megill <megill1352@gmail.comm>
2017-05-02 16:28:38 -0400

    update

--- scripts.js
+++ scripts.js
@@ -13,18 +13,44 @@ $.ajax({
 	console.log(response.posts[i].title);
 
 	chrisApi(response.posts[i]);
+
 }
-
- }
-
+}
 })
 
+function dateChange(date) {
+    var grabDate = document.getElementsByClassName("results")[0];
+    var dateSplit = date.split("/");
+    var month = dateSplit[0];
+    var day = dateSplit[1];
+    var month = "Feb ";
+
+    var date = month + day
+
+    return date;
+
+}
 
 function chrisApi(post) {
     var searchResults = document.getElementsByClassName("results")[0];
-    var resultDiv = "<div class='output'><img src='" + post.img + "'> " + post.date + " " + post.title + "</div>";
+
+    var resultDiv = "<div class='output'><img src='" 
+    resultDiv += post.img 
+    resultDiv += "'> " 
+        resultDiv += "<div class = 'date'>" 
+        resultDiv += dateChange(post.date) 
+        resultDiv += "</div>" 
+    resultDiv += " " 
+        resultDiv += "<div class='title'>" 
+        resultDiv += post.title 
+        resultDiv += "</div></div>";
+    
     searchResults.innerHTML += resultDiv;
 
     console.log(post);
 
 }
+
+
+
+
